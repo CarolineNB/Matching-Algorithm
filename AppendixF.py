d0eaeb1d2f0eb5a3032e5f406d40997efdeebf99
CarolineNB <30878209+CarolineNB@users.noreply.github.com>
2019-10-29 05:23:46 -0400

    Create AppendixF.py

---
+++ AppendixF.py
@@ -0,0 +1,3 @@
+#The following is the scoring metric for the modified basic Gale-Shapley algorithm that matches
+#multiple students to a single internship where students are left unmatched. Also included is its
+#randomization algorithm
